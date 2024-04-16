4b0b15acb9221161be2e845bd404d419720fc023
Dmitry Makarov <mcarrow.mail@gmail.com>
2024-04-16 18:59:11 +0300

    Сборка отделена от тестирования: для сборки не нужно поднимать тестовое окружение. В качестве тестовой базы используется типовая демонстрационная база технологической платформы.

--- Build.cmd
+++ Build.cmd
@@ -2,17 +2,19 @@
 chcp 65001
 
 SET WORKDIR=%cd%
-SET BUILDPATH=%WORKDIR%\Build
+SET BUILDPATH=%WORKDIR%\build
 SET CFE=%BUILDPATH%\YellowQuery.cfe
-SET TESTCONN=/FO:\Tests\ib
+SET BUILDCONN=/F%BUILDPATH%\ib
 
 mkdir %BUILDPATH%
+echo Инициализирую базу для сборки...
+call runner init-dev
 echo Собираю расширение из исходников...
-call runner compileext --ibconnection %TESTCONN% %WORKDIR%\Extensions\YQ YellowQuery
+call runner compileext --ibconnection %BUILDCONN% %WORKDIR%\Extensions\YQ YellowQuery
 echo Применяю расширение...
-call runner updateext --ibconnection %TESTCONN% YellowQuery
+call runner updateext --ibconnection %BUILDCONN% YellowQuery
 echo Выгружаю cfe...
-call runner unloadext --ibconnection %TESTCONN% %CFE% YellowQuery
+call runner unloadext --ibconnection %BUILDCONN% %CFE% YellowQuery
 echo Собираю надстройку Excel...
 cscript .\Build-xlam.vbs
 pause
--- Tests/Init.cmd
+++ Tests/Init.cmd
@@ -2,7 +2,8 @@
 chcp 65001
 
 SET WORKDIR=%cd%
-SET DTPATH=%WORKDIR%\1Cv8.dt
+SET DTPATH=%WORKDIR%\1cv8.dt
+SET CFEPATH=%WORKDIR%\..\Build\YellowQuery.cfe
 SET TESTCONN=/FO:\Tests\ib
 
 echo Монтирую каталог...
@@ -10,4 +11,8 @@ subst o: .\..\
 
 echo Инициализирую тестовую базу...
 call runner restore --ibconnection %TESTCONN% %DTPATH%
+echo Включаю аутентификацию ОС...
+call runner run --ibconnection %TESTCONN% --db-user "Администратор" --execute .\Externals\DataProcessors\SetOSAuthentication\SetOSAuthentication.epf
+echo Загружаю расширение из файла...
+call runner loadext --ibconnection %TESTCONN% --extension YellowQuery --file "%CFEPATH%" --updatedb
 pause
